38c927bbf3db02d7d7159545bcb76fd6bd711195
Saeed Mohammed <saeedmohammed000111@gmail.com>
2026-08-14 08:30:11 +0300

    Create Problem_47.cpp
    Added a comment block explaining the program's purpose.

---
+++ Problem_47.cpp
@@ -0,0 +1,34 @@
+/*
+  =============================================================================
+  Write a program to ask the user to enter Loan Amount and Monthly Installment,
+  then calculate and print how many months are needed to settle the loan.
+  =============================================================================
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+float ReadPositiveNumber(string Message)
+{
+	float Number;
+	cout << Message << endl;
+	cin >> Number;
+	return Number;
+
+}
+
+float CalcNumberOfMonthNeed(float LoanAmount, float MonthlyPayment)
+{
+	return (float) LoanAmount / MonthlyPayment;
+}
+
+
+
+int main()
+{
+	float LoanAmount = ReadPositiveNumber("Please enter Loan Amount?");
+	float MonthlyPayment = ReadPositiveNumber("Please enter Monthly Payment?");
+
+	cout << "you need = " << CalcNumberOfMonthNeed(LoanAmount, MonthlyPayment)<< " Months" << endl;
+	return 0;
+}
